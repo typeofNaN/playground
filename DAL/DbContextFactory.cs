d3ded0d15d2e3ff829c651522ea33d7b8744bae7
DM2N <sixnoodle@163.com>
2018-04-10 19:56:13 +0800

    error

---
+++ DAL/DbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using System.Runtime.Remoting.Messaging;
+
+namespace DAL
+{
+    public class DbContextFactory
+    {
+        public static MvcBookStoreEntities CreateDbContext()
+        {
+            MvcBookStoreEntities dbContext = (MvcBookStoreEntities)CallContext.GetData("dbContext");
+            if (dbContext == null)
+            {
+                dbContext = new MvcBookStoreEntities();
+                CallContext.SetData("dbContext", dbContext);
+            }
+            return dbContext;
+        }
+    }
+}
---
+++ DALFactory/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+using Model;
+using IDAL;
+
+namespace DALFactory
+{
+    public class DataAccess
+    {
+        private static string AssemblyName = ConfigurationManager.AppSettings["Path"].ToString();
+        private static string db = ConfigurationManager.AppSettings["DB"].ToString();
+        public static IBook CreateBook()
+        {
+            string className = AssemblyName + "." + db + "Book";
+            return (IBook)Assembly.Load(AssemblyName).CreateInstance(className);
+        }
+    }
+}
