1c2b30162a6863d8c518145b6357e5dbe8b46d51
typeofNaN <sixnoodle@163.com>
2018-12-04 19:40:36 +0800

    test

--- FrontEnd/judge-time/judge-time.js
+++ FrontEnd/judge-time/judge-time.js
@@ -16,10 +16,12 @@
     b.setMinutes(strb[1]);
     e.setHours(stre[0]);
     e.setMinutes(stre[1]);
+    var t = parseInt(e.getTime());
+    if (b.getTime() > e.getTime()) t = t + 24 * 60 * 60 * 1000;
 
-    (n.getTime() - b.getTime() > 0 && n.getTime() - e.getTime() < 0)
+    (n.getTime() - b.getTime() > 0 && parseInt(n.getTime()) - t < 0)
       ? img.setAttribute('src', '1.png')
       : img.setAttribute('src', '2.png');
   }
-  timeRange("12:00", "8:00");
-})();
+  timeRange("13:00", "8:00");
+})();// 的说法就是大部分几百万
