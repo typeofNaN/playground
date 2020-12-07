80417589b1247bfd2f507c08d497b8992469fba7
typeofNaN <dmdefine6@gmail.com>
2020-12-07 19:23:27 +0800

    feat: 添加预览

---
+++ d2p-admin/precache-manifest.3b1d489664726d4239c02464e2be3a97.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7313891ea9032079f2",
+    "url": "css/app.2e9b0e60.css"
+  },
+  {
+    "revision": "c75acd011d339c0973d4",
+    "url": "css/chunk-elementUI.84bb20f7.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "24e18e5e843b131d70c634b47f9fdc93",
+    "url": "index.html"
+  },
+  {
+    "revision": "9c7313891ea9032079f2",
+    "url": "js/app.6a19a2f6.js"
+  },
+  {
+    "revision": "c7729d441c4ad5f88378",
+    "url": "js/chunk-2d21a3d2.45aa8f44.js"
+  },
+  {
+    "revision": "c75acd011d339c0973d4",
+    "url": "js/chunk-elementUI.a57d12d4.js"
+  },
+  {
+    "revision": "9f0eab731780c21b5bc2",
+    "url": "js/chunk-libs.20713e26.js"
+  },
+  {
+    "revision": "df8381fa2b405a3aacf7",
+    "url": "js/runtime.7fc17385.js"
+  },
+  {
+    "revision": "f8e967fbcd793e172e00cd26a55d154c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
